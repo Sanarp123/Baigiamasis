018781fdfb8a357e2f1fe3ed75222cdcab42d842
Sanarp123 <105873022+Sanarp123@users.noreply.github.com>
2022-10-24 22:05:49 +0300

    Padaryta laboratorijos forma
    Reikia padaryti tvarkinga. Ijungti radio boxus.

---
+++ Forms/lab_form.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <title>Laboratorijos forma</title>
+</head>
+<body>
+    
+<div class="container-lg">
+    <h1>Tyrimo rezultatai</h1>
+    <form method="post" action="" enctype="multipart/form-data">
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="Grezinys" id="Grezinys">
+                        <label class="Objektas1" for="Objekto tipas">Gręžinys</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="Grezinys" id="Grezinys">
+                        <label class="Objektas2" for="Objekto tipas">Šulinys/Šaltinis</label>
+                    </div>
+            <h5>Adresas</h5>
+                    <div class="form-group">
+                        <label for="Cl">Miestas</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Gatvė</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Namo numeris - butas</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    
+        <div class="d-flex flex-column">        
+             <h4>Bendra cheminė sudėtis</h4>
+             <h5>Anijonai</h5>
+                    <div class="form-group">
+                        <label for="Cl">Chloridas</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Sulfatas</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Hidrokarbonatai</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Nitritai</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Nitratai</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+        </div>
+        <div class="d-flex flex-column">            
+            <h5>Katijonai</h5>
+                    <div class="form-group">
+                        <label for="Cl">Natris</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Amonis</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Kalis</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Magnis</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Kalcis</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+        </div>
+            <h5>Kitos analitės</h5>
+                    <div class="form-group">
+                        <label for="Cl">pH</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Permanganato indeksas</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>   <div class="form-group">
+                        <label for="Cl">Savitasis elektros laidis</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>   <div class="form-group">
+                        <label for="Cl">Bendrasis kietumas</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Sausa liekana</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                <!-- GREZINIAMS -->
+                <div class="form-group">
+                        <label for="Cl">Geležis</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Arsenas</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Manganas</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cl">Fluoridai</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>    <div class="form-group">
+                        <label for="Cl">Boras</label>
+                        <input type="text" class="form-control" id="Chloridas" name="Chloridas">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary" name="submitResults">Pateikti</button>
+    </form>
+    </div>
+</body>
+</html>
+
