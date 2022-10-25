51263588ae908fa61fe13cbf9b67d8d2a91303a5
Sanarp123 <105873022+Sanarp123@users.noreply.github.com>
2022-10-25 21:24:33 +0300

    Forma paslepia ir parodo elementus pagal radio pasirinkima

---
+++ Forms/form_script.js
@@ -0,0 +1,25 @@
+document.getElementById("Sulinys").addEventListener("click", FormControl);
+document.getElementById("Grezinys").addEventListener("click", FormControl);
+
+function FormControl() {
+
+var element1 = document.getElementById("greziniai")
+var element2 = document.getElementById("greziniai2")
+
+
+if (document.getElementById("Sulinys").checked) {
+    element1.classList.add("d-none");
+    element2.classList.add("d-none");
+
+    console.log('hello')
+}
+else if (document.getElementById("Grezinys").checked) {
+    element1.classList.remove("d-none");
+    element2.classList.remove("d-none");   
+
+    console.log('bye')
+}
+
+
+}
+             
